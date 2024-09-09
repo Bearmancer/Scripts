8c1b2559bde5b303a8d3457ed570e7cf9a9b78b5
Lance <kanishknishar@outlook.com>
2024-09-09 19:38:48 +0530

    refactor: music script refinements [2024-09-09]
    Original commits:
    - Added batch mp3 torrent creation
    - Correction

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 $ps1Files = Get-ChildItem "C:\Users\Lance\Documents\PowerShell\*.ps1" -Exclude "Microsoft.PowerShell_profile.ps1"
 
+Set-StrictMode -Version Latest
+
 foreach ($file in $ps1Files) {
     . $file.FullName
 }
