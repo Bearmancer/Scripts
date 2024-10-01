701c9d91ba301f0bb358530f4baf22d040ff7caa
Lance <kanishknishar@outlook.com>
2024-10-01 06:29:34 +0530

    Changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 $ps1Files = Get-ChildItem "C:\Users\Lance\Documents\PowerShell\*.ps1" -Exclude "Microsoft.PowerShell_profile.ps1"
-$scripts = "C:\Users\Lance\Documents\Powershell\Python Scripts"
+$scripts = "C:\Users\Lance\Documents\Powershell\python_scripts"
 
 Set-StrictMode -Version Latest
 
