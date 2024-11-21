8cedfa8266a828ca6218258ccaa4836db227b08d
Lance <kanishknishar@outlook.com>
2024-11-21 08:35:03 +0530

    Use print instead of logging

--- python_scripts/video_editing.py
+++ python_scripts/video_editing.py
@@ -1,11 +1,8 @@
 import sys
 import subprocess
 import ffmpeg
-import logging
 from operator import itemgetter
 from pathlib import Path
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 video_extensions = [".mp4", ".mkv", ".ts", ".avi"]
 
@@ -16,15 +13,15 @@ def extract_chapters(video_files):
             probe = ffmpeg.probe(str(video_file), show_chapters=None)
             chapters = probe.get('chapters', [])
         except ffmpeg.Error as e:
-            logging.error(f"Failed to probe {video_file}: {e.stderr.decode()}")
+            print(f"Failed to probe {video_file}: {e.stderr.decode()}")
             continue
 
         if len(chapters) <= 1:
-            logging.info(f"No chapters found in {video_file.name}.")
+            print(f"No chapters found in {video_file.name}.")
             continue
 
         parent_directory = video_file.parent
-        
+
         for chapter_index, chapter in enumerate(chapters, 1):
             formatted_index = f"{chapter_index:02}"
             output_file_name = parent_directory / f"{parent_directory.name} - Chapter {formatted_index}{video_file.suffix}"
@@ -35,9 +32,9 @@ def extract_chapters(video_files):
                     .output(str(output_file_name), c='copy', avoid_negative_ts='make_zero')
                     .run()
                 )
-                logging.info(f"Extracted chapter {formatted_index} from {video_file.name}.")
+                print(f"Extracted chapter {formatted_index} from {video_file.name}.")
             except ffmpeg.Error as e:
-                logging.error(f"Failed to extract chapter {formatted_index} from {video_file}: {e.stderr.decode()}")
+                print(f"Failed to extract chapter {formatted_index} from {video_file}: {e.stderr.decode()}")
 
 
 def batch_compression(path):
@@ -51,29 +48,23 @@ def batch_compression(path):
                 .run()
             )
             file.unlink()
-            logging.info(f"Successfully converted {file}.")
+            print(f"Successfully converted {file}.")
         except ffmpeg.Error as e:
-            logging.error(f"Failed to convert {file}: {e.stderr.decode()}")
+            print(f"Failed to convert {file}: {e.stderr.decode()}")
 
 
 def remux_dvd(path):
-    directories = [path] + [d for d in path.rglob("*") if d.is_dir() and "BACKUP" not in d.name]
-    non_remuxable = []
-
-    for dvd_path in directories:
-        remuxable = [f for f in dvd_path.rglob("*") if f.name in ('VIDEO_TS.IFO', 'index.bdmv')]
-        if remuxable:
-            for file in remuxable:
-                logging.info(f"Converting file: {file} in {dvd_path}")
-                convert_dvd_to_mkv(file, dvd_path)
-        else:
-            logging.info(f"No remuxable files found in {dvd_path}.")
-            non_remuxable.append(dvd_path)
-
-    if non_remuxable:
-        logging.info("Folders that couldn't be remuxed:")
-        for folder in non_remuxable:
-            logging.info(folder)
+    remuxable_files = [
+        f for f in path.rglob('*')
+        if f.name in ('VIDEO_TS.IFO', 'index.bdmv') and 'BACKUP' not in f.parts
+    ]
+    
+    if remuxable_files:
+        for file in remuxable_files:
+            print(f"Converting file: {file}")
+            convert_dvd_to_mkv(file, file.parent)
+    else:
+        print(f"No remuxable files found in {path}.")
 
 
 def convert_dvd_to_mkv(file, dvd_folder):
@@ -83,19 +74,21 @@ def convert_dvd_to_mkv(file, dvd_folder):
     command = [r"C:\Program Files (x86)\MakeMKV\makemkvcon64.exe", "mkv", f'file:{file}', "all", str(dvd_folder), "--minlength=180"]
 
     result = subprocess.run(command, capture_output=True, text=True)
+    
     if result.returncode == 0:
-        logging.info(f"Successfully converted {file}.")
+        print(f"Successfully converted {file}.")
     else:
-        logging.error(f"Failed to convert {file}: {result.stderr}")
+        print(f"Failed to convert {file}: {result.stderr}")
 
 
 def extract_audio_commentary(video_files):
     for file in video_files:
         try:
             probe = ffmpeg.probe(str(file))
-            audio_streams = [stream for stream in probe['streams'] if stream['codec_type'] == 'audio']
+            audio_streams = [stream for stream in probe['streams']
+                             if stream['codec_type'] == 'audio']
         except ffmpeg.Error as e:
-            logging.error(f"Failed to probe {file}: {e.stderr.decode()}")
+            print(f"Failed to probe {file}: {e.stderr.decode()}")
             continue
 
         if len(audio_streams) > 1:
@@ -107,12 +100,12 @@ def extract_audio_commentary(video_files):
                     .output(str(output_file), map='0:a:1', acodec='flac')
                     .run()
                 )
-                logging.info(f"Extracted audio commentary from {file.name}.")
+                print(f"Extracted audio commentary from {file.name}.")
             except ffmpeg.Error as e:
-                logging.error(f"Failed to extract audio commentary from {file}: {e.stderr.decode()}")
+                print(f"Failed to extract audio commentary from {file}: {e.stderr.decode()}")
 
     for flac_file in Path('.').glob('*.flac'):
-        logging.info(flac_file.name)
+        print(flac_file.name)
 
 
 def print_video_resolution(video_files):
@@ -130,30 +123,31 @@ def print_video_resolution(video_files):
         else:
             files_unresolved_resolution.append(file.name)
 
-    logging.info("Files with a resolution of 1920x1080:")
+    print("Files with a resolution of 1920x1080:")
     for name in files_1920_1080:
-        logging.info(name)
+        print(name)
 
-    logging.info("\nFiles with resolution below 1920x1080:")
+    print("\nFiles with resolution below 1920x1080:")
     for name, res in files_below_1920_1080:
-        logging.info(f"{name}, Resolution: {res['Width']}x{res['Height']}")
+        print(f"{name}, Resolution: {res['Width']}x{res['Height']}")
 
-    logging.info("\nFiles with unresolved resolution:")
+    print("\nFiles with unresolved resolution:")
     for name in files_unresolved_resolution:
-        logging.info(name)
+        print(name)
 
 
 def get_video_resolution(filepath):
     try:
         probe = ffmpeg.probe(str(filepath))
-        video_streams = [stream for stream in probe['streams'] if stream['codec_type'] == 'video']
+        video_streams = [stream for stream in probe['streams']
+                         if stream['codec_type'] == 'video']
         width = int(video_streams[0]['width'])
         height = int(video_streams[0]['height'])
         return {"Width": width, "Height": height}
-    
+
     except (ffmpeg.Error, IndexError, KeyError, ValueError) as e:
-        logging.error(f"Error processing file: {filepath}. Error: {e}")
-    
+        print(f"Error processing file: {filepath}. Error: {e}")
+
     return None
 
 
@@ -165,8 +159,9 @@ def calculate_mb_per_minute(file):
         size = float(format_info.get('size', 0))
         mb_per_minute = (size / (1024 * 1024)) / (duration / 60)
         return mb_per_minute, size, duration
+    
     except ffmpeg.Error as e:
-        logging.error(f"Error processing file: {file}. Error: {e.stderr.decode()}")
+        print(f"Error processing file: {file}. Error: {e.stderr.decode()}")
         return 0, 0, 0
 
 
@@ -188,12 +183,12 @@ def calculate_mb_for_directory(video_files):
                 f"Duration: {duration / 60:.2f} minutes\n\n"
             )
 
-    logging.info(f"Output saved as '{output_file_path}'.")
+    print(f"Output saved as '{output_file_path}'.")
 
 
 def main():
     if len(sys.argv) != 3:
-        logging.error("Usage: script.py <Method> <FolderPath>")
+        print("Usage: script.py <Method> <FolderPath>")
         sys.exit(1)
 
     method, folder = sys.argv[1], Path(sys.argv[2])
@@ -212,7 +207,7 @@ def main():
     elif method == "CalculateMBPerMinute":
         calculate_mb_for_directory(video_files)
     else:
-        logging.error("Invalid method specified.")
+        print("Invalid method specified.")
 
 
 if __name__ == "__main__":
