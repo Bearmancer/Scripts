3ada49b97672c8426d7e83f997b24f04bb0be64f
Lance <kanishknishar@outlook.com>
2025-05-01 11:33:28 +0530

    Migrated video_editing to argparse, use switch for pwsh functions not boolean

--- Misc.ps1
+++ Misc.ps1
@@ -2,45 +2,38 @@ function OpenCommandHistory {
     code C:\Users\Lance\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
 }
 
-function PrintVideoResolutions([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function PrintVideoResolutions([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\video_editing.py PrintVideoResolution $directory.FullName
 }
 
-function RemuxDisc ([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
-    Write-Host "Remuxing disc in $directory"
-
-    py C:\Users\Lance\Documents\Powershell\python_scripts\video_editing.py RemuxDisc $directory.FullName
-
-    Write-Host "Remuxing has finished"
+function RemuxDisc([System.IO.DirectoryInfo]$directory = $(Get-Item .), [switch]$get_media_info) {
+    $arg = @("C:\Users\Lance\Documents\Powershell\python_scripts\video_editing.py", "RemuxDisc", $directory.FullName)
+    
+    if ($get_media_info) { $arg += "--get-mediainfo" }
+    
+    py $arg
 }
 
-function BatchCompression ([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function BatchCompression([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\video_editing.py BatchCompression $directory.FullName
 }
 
-function ExtractChapters ([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function ExtractChapters([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\video_editing.py ExtractChapters $directory.FullName
 }
 
-function ListDirectories([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location)), [string]$sort_order = "0") {
+function ListDirectories([System.IO.DirectoryInfo]$directory = $(Get-Item .), [string]$sort_order = "0") {
     py C:\Users\Lance\Documents\Powershell\python_scripts\misc.py list_dir $directory.FullName $sort_order
 }
 
-function ListFilesAndDirectories([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location)), [string]$sort_order = "0") {
+function ListFilesAndDirectories([System.IO.DirectoryInfo]$directory = $(Get-Item .), [string]$sort_order = "0") {
     py C:\Users\Lance\Documents\Powershell\python_scripts\misc.py list_files_and_dirs $directory.FullName $sort_order
 }
 
-function MakeTorrents {[CmdletBinding()] param(
-        [Parameter(Position = 0)]
-        [System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location)),
-
-        [Parameter(Position = 1)]
-        [bool]$recursive = $false
-    )
-
-    if ($recursive) {
-        py C:\Users\Lance\Documents\Powershell\python_scripts\misc.py make_torrents $directory.FullName --recursive
-    } else {
-        py C:\Users\Lance\Documents\Powershell\python_scripts\misc.py make_torrents $directory.FullName
-    }
+function MakeTorrents([System.IO.DirectoryInfo]$directory = $(Get-Item .), [switch]$recursive) {
+    $baseArgs = @("make_torrents", $directory.FullName)
+    
+    if ($recursive) { $baseArgs += "--recursive" }
+    
+    py C:\Users\Lance\Documents\Powershell\python_scripts\misc.py $baseArgs
 }
--- Music.ps1
+++ Music.ps1
@@ -2,32 +2,28 @@ function Propolis {
     C:\Users\Lance\AppData\Local\Personal\Propolis\propolis_windows.exe --no-specs .
 }
 
-function ConvertMusic([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location)), [string]$format = "all") {
+function ConvertMusic([System.IO.DirectoryInfo]$directory = $(Get-Item .), [string]$format = "all") {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_conversion.py $directory -f $format
 }
 
-function ConvertToMP3([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function ConvertToMP3([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_conversion.py -f mp3 $directory.FullName
 }
 
-function ConvertToFLAC([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function ConvertToFLAC([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_conversion.py -f flac $directory.FullName
 }
 
-function RenameFileRed([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function RenameFileRed([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_tools.py -c rfr -d $directory.FullName
 }
 
-function GetEmbeddedImageSize([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function GetEmbeddedImageSize([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_tools.py -c calculate_image_size -d $directory.FullName
 }
 
-function ZipFiles([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location)), $process_all = "True") {
-    py C:\Users\Lance\Documents\Powershell\python_scripts\zip_folders.py $directory.FullName $process_all
-}
-
-function SACDExtract([System.IO.DirectoryInfo]$directory = $(Get-Item (Get-Location))) {
+function SACDExtract([System.IO.DirectoryInfo]$directory = $(Get-Item .)) {
     py C:\Users\Lance\Documents\Powershell\python_scripts\music_conversion.py $directory.FullName
 }
 
-Set-Alias -Name rfr -Value renameFileRed
+Set-Alias -Name rfr -Value RenameFileRed
