3962afef0a004259cd823bb63ab9cf4bb0fe1cf0
kanishknishar <kanishknishar@outlook.com>
2026-05-23 15:51:43 +0530

    feat(t1-01): register all entity DbSets on ScriptsDbContext

--- csharp/src/Data/ScriptsDbContext.cs
+++ csharp/src/Data/ScriptsDbContext.cs
@@ -1,17 +1,33 @@
 using Microsoft.EntityFrameworkCore;
+using CSharpScripts.Data.Entities;
+using Scrobble = CSharpScripts.Data.Entities.Scrobble;
+using Track = CSharpScripts.Data.Entities.Track;
 
 namespace CSharpScripts.Data;
 
 /// <summary>
 /// Primary EF Core DbContext for the Scripts application.
 /// NoTracking is the default; enable tracking explicitly per-operation when needed.
+/// Entity type configurations are loaded from assembly in OnModelCreating.
 /// </summary>
 public sealed class ScriptsDbContext(DbContextOptions<ScriptsDbContext> options)
     : DbContext(options)
 {
+    // Music domain
+    public DbSet<Artist> Artists => Set<Artist>();
+    public DbSet<Album> Albums => Set<Album>();
+    public DbSet<Track> Tracks => Set<Track>();
+    public DbSet<Scrobble> Scrobbles => Set<Scrobble>();
+    public DbSet<Video> Videos => Set<Video>();
+
+    // Management domain
+    public DbSet<ExecutionLog> ExecutionLogs => Set<ExecutionLog>();
+    public DbSet<FailedTask> FailedTasks => Set<FailedTask>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        modelBuilder.Ignore<System.Text.Json.JsonDocument>();
         // Entity configurations will be loaded here in subsequent plans (03-dbcontext-config)
     }
 }
---
+++ csharp/tests/Scripts.Tests/Entities/DbContextDbSetTests.cs
@@ -0,0 +1,67 @@
+using TUnit;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using CSharpScripts.Data;
+using CSharpScripts.Data.Entities;
+
+namespace Scripts.Tests.Entities;
+
+internal sealed class DbContextDbSetTests
+{
+    private static ScriptsDbContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<ScriptsDbContext>()
+            .UseInMemoryDatabase("DbSetTest_" + Guid.NewGuid())
+            .Options;
+        return new ScriptsDbContext(options);
+    }
+
+    [Test]
+    public void DbContext_HasArtists_DbSet()
+    {
+        using var context = CreateContext();
+        context.Artists.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasAlbums_DbSet()
+    {
+        using var context = CreateContext();
+        context.Albums.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasTracks_DbSet()
+    {
+        using var context = CreateContext();
+        context.Tracks.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasScrobbles_DbSet()
+    {
+        using var context = CreateContext();
+        context.Scrobbles.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasVideos_DbSet()
+    {
+        using var context = CreateContext();
+        context.Videos.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasExecutionLogs_DbSet()
+    {
+        using var context = CreateContext();
+        context.ExecutionLogs.Should().NotBeNull();
+    }
+
+    [Test]
+    public void DbContext_HasFailedTasks_DbSet()
+    {
+        using var context = CreateContext();
+        context.FailedTasks.Should().NotBeNull();
+    }
+}
